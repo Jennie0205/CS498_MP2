fde8e98c792ff8afa9c22096645bc548081894a2
Jennie <imyang0205@gmail.com>
2024-01-26 12:43:19 -0800

    Initial commit

---
+++ serve.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import subprocess
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def handle_request():
+    if request.method == 'POST':
+        # Handling HTTP POST request
+        subprocess.Popen(['python', 'stress_cpu.py'])
+        return 'Stressing CPU process started.\n', 200
+    elif request.method == 'GET':
+        # Handling HTTP GET request
+        private_ip = socket.gethostbyname(socket.gethostname())
+        return jsonify({'private_ip': private_ip}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
